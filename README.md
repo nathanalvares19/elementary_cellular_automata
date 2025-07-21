03208ba9a27e43b5f29eff1d27fa6a9667113361
nathanalvares19 <nathanalvares2706@gmail.com>
2025-07-21 12:55:05 +0530

    final commit

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
 # Elementary Cellular Automaton implementation in C
 
+## Instructions
+
+1. Compile the main.c file (gcc must be installed)
+
+```bash
+    gcc --version
+    gcc main.c -o main
+
+```
+
+2. Run the executable
+
+```bash
+    ./main
+
+```
+
+## Setup
+
+1. Change ROW_LENGTH to adjust the width of the cellular automaton (number of cells)
+2. Change TIME_PERIOD to adjust the height of the cellular automaton (number of generations of cells)
+3. Change CURRENT_RULE to any number between 0 - 255 to change the evolution of the cells
+   Refer to [List of CA Rules][3]
+
 ## New Stuff
 
-1. clock arithmetic
+1. Clock arithmetic
+2. Sexagesimal
+3. Bit mask
 
 ## References
 
-1. https://github.com/Julian-Behnaz/Build-your-own-1D-Cellular-Automata
-2. https://www.wikiwand.com/en/articles/Cellular_automaton
-3. https://plato.stanford.edu/entries/cellular-automata/supplement.html
-4. https://www.wikiwand.com/en/articles/Cellular_automaton#Elementary_cellular_automata
+[1]: https://github.com/Julian-Behnaz/Build-your-own-1D-Cellular-Automata
+[2]: https://www.wikiwand.com/en/articles/Cellular_automaton
+[3]: https://plato.stanford.edu/entries/cellular-automata/supplement.html
+[4]: https://www.wikiwand.com/en/articles/Cellular_automaton#Elementary_cellular_automata
--- main.c
+++ main.c
@@ -1,3 +1,58 @@
-#define ROW_LENGTH 5
+#include <stdio.h>
 
-int current_cell_states[ROW_LENGTH];
+#define ROW_LENGTH 50
+#define CURRENT_RULE 204
+#define TIME_PERIOD 20
+
+int DATA[ROW_LENGTH * 2];
+
+int *current_cell_states;
+int *next_cell_states;
+int *temp;
+
+int stateBasedOnNeighbours(int rule, int left, int center, int right)
+{
+    return (rule >> (left << 2 | center << 1 | right)) & 1;
+}
+
+int main(void)
+{
+    // Initialize all cells to zero
+    for (int i = 0; i < ROW_LENGTH * 2; i++)
+    {
+        DATA[i] = 0;
+    }
+
+    current_cell_states = DATA;
+    next_cell_states = DATA + ROW_LENGTH;
+
+    // initial state
+    current_cell_states[ROW_LENGTH / 2] = 1;
+
+    for (int t = 0; t < TIME_PERIOD; t++)
+    {
+        // display the current state
+        for (int i = 0; i < ROW_LENGTH; i++)
+        {
+            printf("%c", current_cell_states[i] ? '#' : '.');
+        }
+        printf("\n");
+
+        // find new states
+        for (int i = 0; i < ROW_LENGTH; i++)
+        {
+            int left, center, right;
+
+            left = current_cell_states[(i - 1 + ROW_LENGTH) % ROW_LENGTH];
+            center = current_cell_states[i];
+            right = current_cell_states[(i + 1) % ROW_LENGTH];
+
+            next_cell_states[i] = stateBasedOnNeighbours(CURRENT_RULE, left, center, right);
+        }
+
+        temp = current_cell_states;
+        current_cell_states = next_cell_states;
+        next_cell_states = temp;
+    }
+    return 0;
+}
